1b6c6f929bc708dce6a07f53843ea9da60acd219
Burgos19 <burgosponce2001@gmail.com>
2025-02-19 18:22:45 -0600

    Agregar archivos de proyecto.

---
+++ ExamenIParcial/Alumno.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ExamenIParcial
+{
+    public class Alumno
+    {
+        public string Nombre {  get; set; }
+        public string NumeroCuenta { get; set; }
+        public string Email {  get; set; }
+
+        public Alumno(string nombre, string numeroCuenta, string email)
+        {
+            Nombre = nombre;
+            NumeroCuenta = numeroCuenta;
+            Email = email;
+
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"Nombre del Alumno: {Nombre}");
+            Console.WriteLine($"Numero de Cuenta: {NumeroCuenta}");
+            Console.WriteLine($"Email: {Email}");
+
+        }
+
+
+    }
+}
---
+++ ExamenIParcial/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExamenIParcial;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el nombre del alumno: ");
+        string nombre = Console.ReadLine();
+
+        Console.Write("Ingrese el número de cuenta: ");
+        string numeroCuenta = Console.ReadLine();
+
+        Console.Write("Ingrese el email del alumno: ");
+        string email = Console.ReadLine();
+
+        Alumno alumno = new Alumno(nombre, numeroCuenta, email);
+        alumno.MostrarDatos();
+    }
+}
